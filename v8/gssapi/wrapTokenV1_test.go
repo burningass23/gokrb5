05f08c7dd28259963f64b0afd8fdf37a4560a68a
Михаил Солодягин <mikhail.solodyagin@tele2.ru>
2023-05-10 22:13:18 +0300

    forking done

--- v8/gssapi/wrapTokenV1_test.go
+++ v8/gssapi/wrapTokenV1_test.go
@@ -1,105 +1,191 @@
 package gssapi
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"testing"
 
-	"github.com/burningass23/gokrb5/v8/types"
+	"github.com/jcmturner/gokrb5/v8/iana/keyusage"
+	"github.com/jcmturner/gokrb5/v8/types"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	kerberosTokenV1        = "603006092a864886f71201020202011100ffffffffec07ef3418bb9d5384c646f1ce946db60c901a9dedf79ea30101000001"
-	replyToKerberosTokenV1 = "603006092a864886f71201020202011100ffffffff3085bf00a417fcf490f1c33c80eee85253734436d6f1048a0101000001"
+	// What a kerberized server might send
+	testChallengeFromAcceptor = "050401ff000c000000000000575e85d601010000853b728d5268525a1386c19f"
+	// What an initiator client could reply
+	testChallengeReplyFromInitiator = "050400ff000c000000000000000000000101000079a033510b6f127212242b97"
+	// session key used to sign the tokens above
+	sessionKey     = "14f9bde6b50ec508201a97f74c4e5bd3"
+	sessionKeyType = 17
 
-	sessionKeyTypeV1       = 23
-	sessionKeyV1           = "c5b294ffa21a9cb1050a13213a88cd7b"
-
-	checksum  = "7ba2b10519ffdc23"
-	sndSeqNum = "bbea237800000000"
+	acceptorSeal  = keyusage.GSSAPI_ACCEPTOR_SEAL
+	initiatorSeal = keyusage.GSSAPI_INITIATOR_SEAL
 )
 
-func getSessionKeyV1(sessionKey string, keyType int32) types.EncryptionKey {
+func getSessionKey() types.EncryptionKey {
 	key, _ := hex.DecodeString(sessionKey)
 	return types.EncryptionKey{
-		KeyType:  keyType,
+		KeyType:  sessionKeyType,
 		KeyValue: key,
 	}
 }
 
-func TestUnmarshal_KerberosTokenV1(t *testing.T) {
-	kerToken, _ := hex.DecodeString(kerberosTokenV1)
-
-	expectedWrapTokenV1 := WrapTokenV1 {}
-	expectedWrapTokenV1.SGN_ALG    = kerToken[15:17]
-	expectedWrapTokenV1.SEAL_ALG   = kerToken[17:19]
-	expectedWrapTokenV1.SndSeqNum  = kerToken[21:29]
-	expectedWrapTokenV1.CheckSum   = kerToken[29:37]
-	expectedWrapTokenV1.Confounder = kerToken[37:45]
-	expectedWrapTokenV1.Payload    = kerToken[45:]
-
-	var wt WrapTokenV1
-	err := wt.Unmarshal(kerToken, false)
-
-	assert.Nil(t, err, "Unexpected error occurred.")
-	assert.Equal(t, &expectedWrapTokenV1, &wt, "Token not decoded as expected.")
+func getChallengeReference() *WrapTokenV1 {
+	challenge, _ := hex.DecodeString(testChallengeFromAcceptor)
+	return &WrapTokenV1{
+		Flags:     0x01,
+		EC:        12,
+		RRC:       0,
+		SndSeqNum: binary.BigEndian.Uint64(challenge[8:16]),
+		Payload:   []byte{0x01, 0x01, 0x00, 0x00},
+		CheckSum:  challenge[20:32],
+	}
 }
 
-func TestVerify_KerberosTokenV1(t *testing.T) {
-	kerToken, _ := hex.DecodeString(kerberosTokenV1)
-
-	var wt WrapTokenV1
-	err := wt.Unmarshal(kerToken, false)
-	assert.Nil(t, err, "Unexpected error occurred.")
-
-	success, err := wt.Verify(getSessionKeyV1(sessionKeyV1, sessionKeyTypeV1), acceptorSign)
-	assert.Nil(t, err, "Unexpected error occurred.")
-	assert.Equal(t, true, success, "Token not decoded as expected.")
+func getChallengeReferenceNoChksum() *WrapTokenV1 {
+	c := getChallengeReference()
+	c.CheckSum = nil
+	return c
 }
 
-// ========================
-func TestUnmarshal_ReplyToKerberosTokenV1(t *testing.T) {
-	kerToken, _ := hex.DecodeString(replyToKerberosTokenV1)
-
-	expectedWrapTokenV1 := WrapTokenV1 {}
-	expectedWrapTokenV1.SGN_ALG    = kerToken[15:17]
-	expectedWrapTokenV1.SEAL_ALG   = kerToken[17:19]
-	expectedWrapTokenV1.SndSeqNum  = kerToken[21:29]
-	expectedWrapTokenV1.CheckSum   = kerToken[29:37]
-	expectedWrapTokenV1.Confounder = kerToken[37:45]
-	expectedWrapTokenV1.Payload    = kerToken[45:]
-
-	var wt WrapTokenV1
-	err := wt.Unmarshal(kerToken, false)
-
-	assert.Nil(t, err, "Unexpected error occurred.")
-	assert.Equal(t, &expectedWrapTokenV1, &wt, "Token not decoded as expected.")
+func getResponseReference() *WrapTokenV1 {
+	response, _ := hex.DecodeString(testChallengeReplyFromInitiator)
+	return &WrapTokenV1{
+		Flags:     0x00,
+		EC:        12,
+		RRC:       0,
+		SndSeqNum: 0,
+		Payload:   []byte{0x01, 0x01, 0x00, 0x00},
+		CheckSum:  response[20:32],
+	}
 }
 
-func TestVerify_ReplyToKerberosTokenV1(t *testing.T) {
-	kerToken, _ := hex.DecodeString(replyToKerberosTokenV1)
-
-	var wt WrapTokenV1
-	err := wt.Unmarshal(kerToken, false)
-	assert.Nil(t, err, "Unexpected error occurred.")
-
-	success, err := wt.Verify(getSessionKeyV1(sessionKeyV1, sessionKeyTypeV1), acceptorSign)
-	assert.Nil(t, err, "Unexpected error occurred.")
-	assert.Equal(t, true, success, "Token not decoded as expected.")
+func getResponseReferenceNoChkSum() *WrapTokenV1 {
+	r := getResponseReference()
+	r.CheckSum = nil
+	return r
 }
 
-// ========================
-func TestEncodeDecodeSndSeqNum(t *testing.T) {
-	session, _ := hex.DecodeString(sessionKeyV1)
-	check, _   := hex.DecodeString(checksum)
-	ssn, _     := hex.DecodeString(sndSeqNum)
+func TestUnmarshal_Challenge(t *testing.T) {
+	t.Parallel()
+	challenge, _ := hex.DecodeString(testChallengeFromAcceptor)
+	var wt WrapTokenV1
+	err := wt.Unmarshal(challenge, true)
+	assert.Nil(t, err, "Unexpected error occurred.")
+	assert.Equal(t, getChallengeReference(), &wt, "Token not decoded as expected.")
+}
 
-	expectedSndSeqNum := []byte{0x93, 0x4b, 0x9a, 0x21, 0xc8, 0x23, 0xf8, 0x98}
+func TestUnmarshalFailure_Challenge(t *testing.T) {
+	t.Parallel()
+	challenge, _ := hex.DecodeString(testChallengeFromAcceptor)
+	var wt WrapTokenV1
+	err := wt.Unmarshal(challenge, false)
+	assert.NotNil(t, err, "Expected error did not occur: a message from the acceptor cannot be expected to be sent from the initiator.")
+	assert.Nil(t, wt.Payload, "Token fields should not have been initialised")
+	assert.Nil(t, wt.CheckSum, "Token fields should not have been initialised")
+	assert.Equal(t, byte(0x00), wt.Flags, "Token fields should not have been initialised")
+	assert.Equal(t, uint16(0), wt.EC, "Token fields should not have been initialised")
+	assert.Equal(t, uint16(0), wt.RRC, "Token fields should not have been initialised")
+	assert.Equal(t, uint64(0), wt.SndSeqNum, "Token fields should not have been initialised")
+}
 
-	wrapTokenV1           := WrapTokenV1 {}
-	wrapTokenV1.SndSeqNum  = ssn
+func TestUnmarshal_ChallengeReply(t *testing.T) {
+	t.Parallel()
+	response, _ := hex.DecodeString(testChallengeReplyFromInitiator)
+	var wt WrapTokenV1
+	err := wt.Unmarshal(response, false)
+	assert.Nil(t, err, "Unexpected error occurred.")
+	assert.Equal(t, getResponseReference(), &wt, "Token not decoded as expected.")
+}
 
-	wrapTokenV1.encryptSndSeqNum(session, check)
+func TestUnmarshalFailure_ChallengeReply(t *testing.T) {
+	t.Parallel()
+	response, _ := hex.DecodeString(testChallengeReplyFromInitiator)
+	var wt WrapTokenV1
+	err := wt.Unmarshal(response, true)
+	assert.NotNil(t, err, "Expected error did not occur: a message from the initiator cannot be expected to be sent from the acceptor.")
+	assert.Nil(t, wt.Payload, "Token fields should not have been initialised")
+	assert.Nil(t, wt.CheckSum, "Token fields should not have been initialised")
+	assert.Equal(t, byte(0x00), wt.Flags, "Token fields should not have been initialised")
+	assert.Equal(t, uint16(0), wt.EC, "Token fields should not have been initialised")
+	assert.Equal(t, uint16(0), wt.RRC, "Token fields should not have been initialised")
+	assert.Equal(t, uint64(0), wt.SndSeqNum, "Token fields should not have been initialised")
+}
 
-	assert.Equal(t, expectedSndSeqNum, wrapTokenV1.SndSeqNum, "SndSeqNum is not correctly encrypted")
+func TestChallengeChecksumVerification(t *testing.T) {
+	t.Parallel()
+	challenge, _ := hex.DecodeString(testChallengeFromAcceptor)
+	var wt WrapTokenV1
+	wt.Unmarshal(challenge, true)
+	challengeOk, cErr := wt.Verify(getSessionKey(), acceptorSeal)
+	assert.Nil(t, cErr, "Error occurred during checksum verification.")
+	assert.True(t, challengeOk, "Checksum verification failed.")
+}
+
+func TestResponseChecksumVerification(t *testing.T) {
+	t.Parallel()
+	reply, _ := hex.DecodeString(testChallengeReplyFromInitiator)
+	var wt WrapTokenV1
+	wt.Unmarshal(reply, false)
+	replyOk, rErr := wt.Verify(getSessionKey(), initiatorSeal)
+	assert.Nil(t, rErr, "Error occurred during checksum verification.")
+	assert.True(t, replyOk, "Checksum verification failed.")
+}
+
+func TestChecksumVerificationFailure(t *testing.T) {
+	t.Parallel()
+	challenge, _ := hex.DecodeString(testChallengeFromAcceptor)
+	var wt WrapTokenV1
+	wt.Unmarshal(challenge, true)
+
+	// Test a failure with the correct key but wrong keyusage:
+	challengeOk, cErr := wt.Verify(getSessionKey(), initiatorSeal)
+	assert.NotNil(t, cErr, "Expected error did not occur.")
+	assert.False(t, challengeOk, "Checksum verification succeeded when it should have failed.")
+
+	wrongKeyVal, _ := hex.DecodeString("14f9bde6b50ec508201a97f74c4effff")
+	badKey := types.EncryptionKey{
+		KeyType:  sessionKeyType,
+		KeyValue: wrongKeyVal,
+	}
+	// Test a failure with the wrong key but correct keyusage:
+	wrongKeyOk, wkErr := wt.Verify(badKey, acceptorSeal)
+	assert.NotNil(t, wkErr, "Expected error did not occur.")
+	assert.False(t, wrongKeyOk, "Checksum verification succeeded when it should have failed.")
+}
+
+func TestMarshal_Challenge(t *testing.T) {
+	t.Parallel()
+	bytes, _ := getChallengeReference().Marshal()
+	assert.Equal(t, testChallengeFromAcceptor, hex.EncodeToString(bytes),
+		"Marshalling did not yield the expected result.")
+}
+
+func TestMarshal_ChallengeReply(t *testing.T) {
+	t.Parallel()
+	bytes, _ := getResponseReference().Marshal()
+	assert.Equal(t, testChallengeReplyFromInitiator, hex.EncodeToString(bytes),
+		"Marshalling did not yield the expected result.")
+}
+
+func TestMarshal_Failures(t *testing.T) {
+	t.Parallel()
+	noChkSum := getResponseReferenceNoChkSum()
+	chkBytes, chkErr := noChkSum.Marshal()
+	assert.Nil(t, chkBytes, "No bytes should be returned.")
+	assert.NotNil(t, chkErr, "Expected an error as no checksum was set")
+
+	noPayload := getResponseReference()
+	noPayload.Payload = nil
+	pldBytes, pldErr := noPayload.Marshal()
+	assert.Nil(t, pldBytes, "No bytes should be returned.")
+	assert.NotNil(t, pldErr, "Expected an error as no checksum was set")
+}
+
+func TestNewInitiatorTokenSignatureAndMarshalling(t *testing.T) {
+	t.Parallel()
+	token, tErr := NewInitiatorWrapTokenV1([]byte{0x01, 0x01, 0x00, 0x00}, getSessionKey())
+	assert.Nil(t, tErr, "Unexpected error.")
+	assert.Equal(t, getResponseReference(), token, "Token failed to be marshalled to the expected bytes.")
 }
--- v8/gssapi/wrapToken_test.go
+++ v8/gssapi/wrapToken_test.go
@@ -1,191 +1,105 @@
 package gssapi
 
 import (
-	"encoding/binary"
 	"encoding/hex"
 	"testing"
 
-	"github.com/burningass23/gokrb5/v8/iana/keyusage"
 	"github.com/burningass23/gokrb5/v8/types"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	// What a kerberized server might send
-	testChallengeFromAcceptor = "050401ff000c000000000000575e85d601010000853b728d5268525a1386c19f"
-	// What an initiator client could reply
-	testChallengeReplyFromInitiator = "050400ff000c000000000000000000000101000079a033510b6f127212242b97"
-	// session key used to sign the tokens above
-	sessionKey     = "14f9bde6b50ec508201a97f74c4e5bd3"
-	sessionKeyType = 17
+	kerberosTokenV1        = "603006092a864886f71201020202011100ffffffffec07ef3418bb9d5384c646f1ce946db60c901a9dedf79ea30101000001"
+	replyToKerberosTokenV1 = "603006092a864886f71201020202011100ffffffff3085bf00a417fcf490f1c33c80eee85253734436d6f1048a0101000001"
 
-	acceptorSeal  = keyusage.GSSAPI_ACCEPTOR_SEAL
-	initiatorSeal = keyusage.GSSAPI_INITIATOR_SEAL
+	sessionKeyTypeV1       = 23
+	sessionKeyV1           = "c5b294ffa21a9cb1050a13213a88cd7b"
+
+	checksum  = "7ba2b10519ffdc23"
+	sndSeqNum = "bbea237800000000"
 )
 
-func getSessionKey() types.EncryptionKey {
+func getSessionKeyV1(sessionKey string, keyType int32) types.EncryptionKey {
 	key, _ := hex.DecodeString(sessionKey)
 	return types.EncryptionKey{
-		KeyType:  sessionKeyType,
+		KeyType:  keyType,
 		KeyValue: key,
 	}
 }
 
-func getChallengeReference() *WrapToken {
-	challenge, _ := hex.DecodeString(testChallengeFromAcceptor)
-	return &WrapToken{
-		Flags:     0x01,
-		EC:        12,
-		RRC:       0,
-		SndSeqNum: binary.BigEndian.Uint64(challenge[8:16]),
-		Payload:   []byte{0x01, 0x01, 0x00, 0x00},
-		CheckSum:  challenge[20:32],
-	}
-}
+func TestUnmarshal_KerberosTokenV1(t *testing.T) {
+	kerToken, _ := hex.DecodeString(kerberosTokenV1)
 
-func getChallengeReferenceNoChksum() *WrapToken {
-	c := getChallengeReference()
-	c.CheckSum = nil
-	return c
-}
+	expectedWrapToken := WrapToken {}
+	expectedWrapToken.SGN_ALG    = kerToken[15:17]
+	expectedWrapToken.SEAL_ALG   = kerToken[17:19]
+	expectedWrapToken.SndSeqNum  = kerToken[21:29]
+	expectedWrapToken.CheckSum   = kerToken[29:37]
+	expectedWrapToken.Confounder = kerToken[37:45]
+	expectedWrapToken.Payload    = kerToken[45:]
 
-func getResponseReference() *WrapToken {
-	response, _ := hex.DecodeString(testChallengeReplyFromInitiator)
-	return &WrapToken{
-		Flags:     0x00,
-		EC:        12,
-		RRC:       0,
-		SndSeqNum: 0,
-		Payload:   []byte{0x01, 0x01, 0x00, 0x00},
-		CheckSum:  response[20:32],
-	}
-}
-
-func getResponseReferenceNoChkSum() *WrapToken {
-	r := getResponseReference()
-	r.CheckSum = nil
-	return r
-}
-
-func TestUnmarshal_Challenge(t *testing.T) {
-	t.Parallel()
-	challenge, _ := hex.DecodeString(testChallengeFromAcceptor)
 	var wt WrapToken
-	err := wt.Unmarshal(challenge, true)
+	err := wt.Unmarshal(kerToken, false)
+
 	assert.Nil(t, err, "Unexpected error occurred.")
-	assert.Equal(t, getChallengeReference(), &wt, "Token not decoded as expected.")
+	assert.Equal(t, &expectedWrapToken, &wt, "Token not decoded as expected.")
 }
 
-func TestUnmarshalFailure_Challenge(t *testing.T) {
-	t.Parallel()
-	challenge, _ := hex.DecodeString(testChallengeFromAcceptor)
-	var wt WrapToken
-	err := wt.Unmarshal(challenge, false)
-	assert.NotNil(t, err, "Expected error did not occur: a message from the acceptor cannot be expected to be sent from the initiator.")
-	assert.Nil(t, wt.Payload, "Token fields should not have been initialised")
-	assert.Nil(t, wt.CheckSum, "Token fields should not have been initialised")
-	assert.Equal(t, byte(0x00), wt.Flags, "Token fields should not have been initialised")
-	assert.Equal(t, uint16(0), wt.EC, "Token fields should not have been initialised")
-	assert.Equal(t, uint16(0), wt.RRC, "Token fields should not have been initialised")
-	assert.Equal(t, uint64(0), wt.SndSeqNum, "Token fields should not have been initialised")
-}
+func TestVerify_KerberosTokenV1(t *testing.T) {
+	kerToken, _ := hex.DecodeString(kerberosTokenV1)
 
-func TestUnmarshal_ChallengeReply(t *testing.T) {
-	t.Parallel()
-	response, _ := hex.DecodeString(testChallengeReplyFromInitiator)
 	var wt WrapToken
-	err := wt.Unmarshal(response, false)
+	err := wt.Unmarshal(kerToken, false)
 	assert.Nil(t, err, "Unexpected error occurred.")
-	assert.Equal(t, getResponseReference(), &wt, "Token not decoded as expected.")
+
+	success, err := wt.Verify(getSessionKeyV1(sessionKeyV1, sessionKeyTypeV1), acceptorSign)
+	assert.Nil(t, err, "Unexpected error occurred.")
+	assert.Equal(t, true, success, "Token not decoded as expected.")
 }
 
-func TestUnmarshalFailure_ChallengeReply(t *testing.T) {
-	t.Parallel()
-	response, _ := hex.DecodeString(testChallengeReplyFromInitiator)
+// ========================
+func TestUnmarshal_ReplyToKerberosTokenV1(t *testing.T) {
+	kerToken, _ := hex.DecodeString(replyToKerberosTokenV1)
+
+	expectedWrapToken := WrapToken {}
+	expectedWrapToken.SGN_ALG    = kerToken[15:17]
+	expectedWrapToken.SEAL_ALG   = kerToken[17:19]
+	expectedWrapToken.SndSeqNum  = kerToken[21:29]
+	expectedWrapToken.CheckSum   = kerToken[29:37]
+	expectedWrapToken.Confounder = kerToken[37:45]
+	expectedWrapToken.Payload    = kerToken[45:]
+
 	var wt WrapToken
-	err := wt.Unmarshal(response, true)
-	assert.NotNil(t, err, "Expected error did not occur: a message from the initiator cannot be expected to be sent from the acceptor.")
-	assert.Nil(t, wt.Payload, "Token fields should not have been initialised")
-	assert.Nil(t, wt.CheckSum, "Token fields should not have been initialised")
-	assert.Equal(t, byte(0x00), wt.Flags, "Token fields should not have been initialised")
-	assert.Equal(t, uint16(0), wt.EC, "Token fields should not have been initialised")
-	assert.Equal(t, uint16(0), wt.RRC, "Token fields should not have been initialised")
-	assert.Equal(t, uint64(0), wt.SndSeqNum, "Token fields should not have been initialised")
+	err := wt.Unmarshal(kerToken, false)
+
+	assert.Nil(t, err, "Unexpected error occurred.")
+	assert.Equal(t, &expectedWrapToken, &wt, "Token not decoded as expected.")
 }
 
-func TestChallengeChecksumVerification(t *testing.T) {
-	t.Parallel()
-	challenge, _ := hex.DecodeString(testChallengeFromAcceptor)
+func TestVerify_ReplyToKerberosTokenV1(t *testing.T) {
+	kerToken, _ := hex.DecodeString(replyToKerberosTokenV1)
+
 	var wt WrapToken
-	wt.Unmarshal(challenge, true)
-	challengeOk, cErr := wt.Verify(getSessionKey(), acceptorSeal)
-	assert.Nil(t, cErr, "Error occurred during checksum verification.")
-	assert.True(t, challengeOk, "Checksum verification failed.")
+	err := wt.Unmarshal(kerToken, false)
+	assert.Nil(t, err, "Unexpected error occurred.")
+
+	success, err := wt.Verify(getSessionKeyV1(sessionKeyV1, sessionKeyTypeV1), acceptorSign)
+	assert.Nil(t, err, "Unexpected error occurred.")
+	assert.Equal(t, true, success, "Token not decoded as expected.")
 }
 
-func TestResponseChecksumVerification(t *testing.T) {
-	t.Parallel()
-	reply, _ := hex.DecodeString(testChallengeReplyFromInitiator)
-	var wt WrapToken
-	wt.Unmarshal(reply, false)
-	replyOk, rErr := wt.Verify(getSessionKey(), initiatorSeal)
-	assert.Nil(t, rErr, "Error occurred during checksum verification.")
-	assert.True(t, replyOk, "Checksum verification failed.")
-}
+// ========================
+func TestEncodeDecodeSndSeqNum(t *testing.T) {
+	session, _ := hex.DecodeString(sessionKeyV1)
+	check, _   := hex.DecodeString(checksum)
+	ssn, _     := hex.DecodeString(sndSeqNum)
 
-func TestChecksumVerificationFailure(t *testing.T) {
-	t.Parallel()
-	challenge, _ := hex.DecodeString(testChallengeFromAcceptor)
-	var wt WrapToken
-	wt.Unmarshal(challenge, true)
+	expectedSndSeqNum := []byte{0x93, 0x4b, 0x9a, 0x21, 0xc8, 0x23, 0xf8, 0x98}
 
-	// Test a failure with the correct key but wrong keyusage:
-	challengeOk, cErr := wt.Verify(getSessionKey(), initiatorSeal)
-	assert.NotNil(t, cErr, "Expected error did not occur.")
-	assert.False(t, challengeOk, "Checksum verification succeeded when it should have failed.")
+	WrapToken           := WrapToken {}
+	WrapToken.SndSeqNum  = ssn
 
-	wrongKeyVal, _ := hex.DecodeString("14f9bde6b50ec508201a97f74c4effff")
-	badKey := types.EncryptionKey{
-		KeyType:  sessionKeyType,
-		KeyValue: wrongKeyVal,
-	}
-	// Test a failure with the wrong key but correct keyusage:
-	wrongKeyOk, wkErr := wt.Verify(badKey, acceptorSeal)
-	assert.NotNil(t, wkErr, "Expected error did not occur.")
-	assert.False(t, wrongKeyOk, "Checksum verification succeeded when it should have failed.")
-}
+	WrapToken.encryptSndSeqNum(session, check)
 
-func TestMarshal_Challenge(t *testing.T) {
-	t.Parallel()
-	bytes, _ := getChallengeReference().Marshal()
-	assert.Equal(t, testChallengeFromAcceptor, hex.EncodeToString(bytes),
-		"Marshalling did not yield the expected result.")
-}
-
-func TestMarshal_ChallengeReply(t *testing.T) {
-	t.Parallel()
-	bytes, _ := getResponseReference().Marshal()
-	assert.Equal(t, testChallengeReplyFromInitiator, hex.EncodeToString(bytes),
-		"Marshalling did not yield the expected result.")
-}
-
-func TestMarshal_Failures(t *testing.T) {
-	t.Parallel()
-	noChkSum := getResponseReferenceNoChkSum()
-	chkBytes, chkErr := noChkSum.Marshal()
-	assert.Nil(t, chkBytes, "No bytes should be returned.")
-	assert.NotNil(t, chkErr, "Expected an error as no checksum was set")
-
-	noPayload := getResponseReference()
-	noPayload.Payload = nil
-	pldBytes, pldErr := noPayload.Marshal()
-	assert.Nil(t, pldBytes, "No bytes should be returned.")
-	assert.NotNil(t, pldErr, "Expected an error as no checksum was set")
-}
-
-func TestNewInitiatorTokenSignatureAndMarshalling(t *testing.T) {
-	t.Parallel()
-	token, tErr := NewInitiatorWrapToken([]byte{0x01, 0x01, 0x00, 0x00}, getSessionKey())
-	assert.Nil(t, tErr, "Unexpected error.")
-	assert.Equal(t, getResponseReference(), token, "Token failed to be marshalled to the expected bytes.")
+	assert.Equal(t, expectedSndSeqNum, WrapToken.SndSeqNum, "SndSeqNum is not correctly encrypted")
 }
